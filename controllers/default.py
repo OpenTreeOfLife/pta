16588d600d9d51fcdb26e715364a1c8852978e08
rhr <github@rree.fastmail.fm>
2014-11-13 15:41:06 -0600

    basic default/index.html functionality

--- controllers/default.py
+++ controllers/default.py
@@ -9,11 +9,24 @@ def index():
     if form.process().accepted:
         fp = os.path.join('/tmp', form.vars.data)
         d = open(fp).read()
+
+    try:
+        session.filename = request.vars.data.filename
+    except:
+        session.filename = None
     
     if not d:
         d = request.vars.data
     if d:
         data = treegraph.nexson2ptag(d)
     if data:
-        print data.get('treeids')
+        for treeid in data.get('treeids'):
+            k = 'pta_{}'.format(treeid)
+            session[k] = data[treeid]
     return dict(data=data, form=form)
+
+def view():
+    treeid = request.vars.treeid
+    k = 'pta_{}'.format(treeid)
+    d = session.get(k)
+    return dict(data=d)
